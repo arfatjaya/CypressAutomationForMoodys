288cc338465eaf832cf6420a8b0a4eed1733fd01
arfatjaya <jayaarfat123@gmail.com>
2025-09-26 11:08:17 +0530

    Initial commit

---
+++ cypress/integration/Test.js
@@ -0,0 +1,5 @@
+describe('Launch URL Test', () => {
+  it('should launch the application URL', () => {
+    cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/'); // Replace with your target URL
+  });
+});
