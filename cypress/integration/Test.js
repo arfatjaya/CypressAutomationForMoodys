0fd2a5c0de1e7f9dbe92a9887b4ef25d61a7b3ed
arfatjaya <jayaarfat123@gmail.com>
2025-09-26 18:36:38 +0530

    Cypress test: add comments and fix selectors

--- cypress/integration/Test.js
+++ cypress/integration/Test.js
@@ -2,6 +2,7 @@ describe('End to End testing', () => { // Test suite for end-to-end flow
   it('should launch the application URL and submit order', () => { // Test case for launching URL and submitting order
     const products = 'Nokia Edge'; // Product to be selected
     let sum = 0; // Variable to store total amount
+    let finalAmount = 0; // Variable to store final amount
 
     cy.visit('https://rahulshettyacademy.com/loginpagePractise/#'); // Visit the login page
     cy.get('#username').type('rahulshettyacademy'); // Enter username
@@ -21,16 +22,34 @@ describe('End to End testing', () => { // Test suite for end-to-end flow
     cy.get('app-card').eq(0).find('button').click(); // Add the first product card to cart
     cy.contains('Checkout').click(); // Click the 'Checkout' button
 
-    cy.get('tr td:nth-child(4) strong').each($el => { // Iterate over each product price in the cart
+    cy.get('tr td:nth-child(4) strong')
+      .each($el => {
+        const amount = Number($el.text().split(" ")[1].trim()) // Extract and convert price to number
+        sum += amount; // Add price to total sum
 
 
-      const amount = Number($el.text().split(" ")[1].trim()) // Extract and convert price to number
-      sum += amount; // Add price to total sum
-    }).then(() => {
-      expect(sum).to.be.lessThan(280000) // Assert total sum is less than 280000
+      }).then(() => {
 
-    })
+        expect(sum).to.be.lessThan(280000) // Assert total sum is less than 280000
 
-      ;
+      });
+    cy.get('tr td:nth-child(5) strong').then($el => {
+      finalAmount = Number($el.text().split(" ")[1].trim())
+
+      expect(finalAmount).to.equal(sum)
+
+
+    });
+
+    //----------------------------------------------------------
+    cy.get('button.btn-success').click();
+    cy.get('#country').type('India');
+    cy.get('div.suggestions li a', { timeout: 10000 }).should('be.visible').click();
+
+    cy.get('input.btn-success').click();
+    cy.get('.alert').should('include.text', 'Success');
+
+
+
   });
 });
